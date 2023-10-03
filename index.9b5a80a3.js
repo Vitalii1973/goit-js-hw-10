1b1ca172d1663058e21e630d492cee11276ee4f5
Vitalii1973 <133043160+Vitalii1973@users.noreply.github.com>
2023-10-03 22:47:35 +0000

    Deploying to gh-pages from @ Vitalii1973/goit-js-hw-10@83fbbff31773c2ee18eac97b0f05e18a1b439761 🚀

---
+++ index.9b5a80a3.js
@@ -0,0 +1,7 @@
+!function(){var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},l={},t={},n=e.parcelRequired7c6;null==n&&((n=function(e){if(e in l)return l[e].exports;if(e in t){var n=t[e];delete t[e];var o={id:e,exports:{}};return l[e]=o,n.call(o.exports,o,o.exports),o.exports}var r=Error("Cannot find module '"+e+"'");throw r.code="MODULE_NOT_FOUND",r}).register=function(e,l){t[e]=l},e.parcelRequired7c6=n);var o=n("eWrZJ");let r=document.getElementById("breedSelect"),d=document.querySelector(".loader"),a=document.querySelector(".error"),i=document.querySelector("#catInfo");// Сховати повідомлення про помилку при завантаженні сторінки
+a.style.display="none",(0,o.fetchBreeds)(),r.addEventListener("change",async()=>{let e=r.value;// Сховати повідомлення про помилку і показати лоадер
+// error.style.display = 'none';
+d.style.display="block",i.style.display="none";try{await (0,o.fetchCatByBreed)(e)}catch(e){// Показати повідомлення про помилку
+a.style.display="block",d.style.display="none",i.style.display="none"}finally{d.style.display="none"}i.style.display="block"})}();//# sourceMappingURL=index.9b5a80a3.js.map
+
+//# sourceMappingURL=index.9b5a80a3.js.map
